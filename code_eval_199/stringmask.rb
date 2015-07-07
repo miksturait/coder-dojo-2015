f8ed9f55a8111892b9e1c39db0ae7a68b129c204
Michal Czyz <michalczyz@gmail.com>
2015-07-07 17:37:01 +0200

    String mask challange. Added default parametr in ProcessFile class.

--- code_eval_199/stringmask.rb
+++ code_eval_199/stringmask.rb
@@ -23,12 +23,12 @@ class StringUppercaseWithMask
 end
 
 class ProcessFile
-  def initialize(filename)
+  def initialize(filename = ARGV[0])
     File.open(filename, "r").each_line { |line| yield(line) }
   end
 end
 
-ProcessFile.new(ARGV[0]) do |line|
+ProcessFile.new do |line|
   puts StringUppercaseWithMask.new(line)
 end
 
