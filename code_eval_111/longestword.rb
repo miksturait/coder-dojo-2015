0558f3816b471d0622f532549b0002aab7e220ca
Michal Czyz <michalczyz@gmail.com>
2015-07-07 16:10:48 +0200

    Longest word challange. Added input from file.

--- code_eval_111/longestword.rb
+++ code_eval_111/longestword.rb
@@ -1,11 +1,6 @@
-sentance_one = 'some line with text'
-sentance_two = 'another line something'
-
-# find the first longest word in sentance
-
 
 def longest_word(sentance)
   sentance.split(' ').max { |a, b| a.length <=> b.length }
 end
 
-[sentance_one, sentance_two].each { |sentance| puts longest_word(sentance) }
+File.open("input.txt", "r").each_line { |line| puts longest_word(line) }
