a176ea180163c4ff28ed9c2438ade9446797e640
Michal Czyz <michalczyz@gmail.com>
2015-07-07 16:14:01 +0200

    Longest word challange. Added input from seleted file.

--- code_eval_111/longestword.rb
+++ code_eval_111/longestword.rb
@@ -1,6 +1,5 @@
-
 def longest_word(sentance)
   sentance.split(' ').max { |a, b| a.length <=> b.length }
 end
 
-File.open("input.txt", "r").each_line { |line| puts longest_word(line) }
+File.open(ARGV[0], "r").each_line { |line| puts longest_word(line) }
