fc0a7fcdbf56552db33b1583489cdeeb3f4f3fb3
Michal Czyz <michalczyz@gmail.com>
2015-07-07 16:02:54 +0200

    Longest word challange. Clean up code duplication.

--- code_eval_111/longestword.rb
+++ code_eval_111/longestword.rb
@@ -1,7 +1,11 @@
 sentance_one = 'some line with text'
-sentance_two = 'another line'
+sentance_two = 'another line something'
 
 # find the first longest word in sentance
 
-puts sentance_one.split(' ').max {|a, b| a.length <=> b.length}
-puts sentance_one.split(' ').max {|a, b| a.length <=> b.length}
+
+def longest_word(sentance)
+  sentance.split(' ').max { |a, b| a.length <=> b.length }
+end
+
+[sentance_one, sentance_two].each { |sentance| puts longest_word(sentance) }
