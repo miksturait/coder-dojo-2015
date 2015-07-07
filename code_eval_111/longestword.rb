4f4eda6e57bf9c4bfd9e4fc5a93aa6663d362fda
Michal Czyz <michalczyz@gmail.com>
2015-07-07 17:42:15 +0200

    String mask challange. Refactoring - DRY in the code!

--- code_eval_111/longestword.rb
+++ code_eval_111/longestword.rb
@@ -1,11 +1,7 @@
+require '../support/process_file'
+
 def longest_word(sentance)
   sentance.split(' ').max { |a, b| a.length <=> b.length }
-end
-
-class ProcessFile
-  def initialize(filename = ARGV[0])
-    File.open(filename, "r").each_line { |line| yield(line) }
-  end
 end
 
 ProcessFile.new do |line|
