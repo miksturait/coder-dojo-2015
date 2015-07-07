ddcb75b20c96dbf33bfdd776d89c41a9e5b270d3
Michal Czyz <michalczyz@gmail.com>
2015-07-07 17:11:36 +0200

    String mask challange. Created class and move existing code inside the class.

--- code_eval_199/stringmask.rb
+++ code_eval_199/stringmask.rb
@@ -1,14 +1,21 @@
-def apply_mask(word, mask)
+class StringUppercaseWithMask
+  attr_reader :word, :mask
 
-  word_splited = word.split(//)
+  def initialize(line)
+    @word, @mask = line.split(' ')
+  end
 
-  word_splited.each_with_index do |letter, index|
-    if mask[index] == '1'
-      word_splited[index].upcase!
-    end
-  end.join
+  def apply_mask()
+
+    word_splited = word.split(//)
+
+    word_splited.each_with_index do |letter, index|
+      if mask[index] == '1'
+        word_splited[index].upcase!
+      end
+    end.join
+  end
+
 end
 
-File.open(ARGV[0], "r").each_line { |line| puts apply_mask(*line.split(' ')) }
-
-
+File.open(ARGV[0], "r").each_line { |line| puts StringUppercaseWithMask.new(line).apply_mask }
