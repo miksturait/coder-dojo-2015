ed0b9765aad87d8b829988c9efae8a3da37a82a5
Michal Czyz <michalczyz@gmail.com>
2015-07-07 19:14:41 +0200

    Text Trimmer Refactored again.

--- code_eval_167/text_trimmer.rb
+++ code_eval_167/text_trimmer.rb
@@ -1,27 +1,34 @@
 text = "Two thousand verses is a great many - very, very great many."
+class TextTrimmer
+  attr_reader :line
 
-def word_length(trimmed_text)
-  (trimmed_text.map(&:length).reduce(:+) || 0) + trimmed_text.size
-end
+  def initialize line
+    @line=line
+  end
 
-def collect_text(text, trimmed_text)
-  text.split(" ").each do |word|
-    if (word_length(trimmed_text) + word.length) < 40
-      trimmed_text.push(word)
+  def text_trim
+    if line.length > 55
+      trimmed_text = []
+      collect_text(line, trimmed_text)
+      trimmed_text.join(" ") << "... <Read More>"
     else
-      break
+      line
+    end
+  end
+
+  def word_length(trimmed_text)
+    (trimmed_text.map(&:length).reduce(:+) || 0) + trimmed_text.size
+  end
+
+  def collect_text(text, trimmed_text)
+    text.split(" ").each do |word|
+      if (word_length(trimmed_text) + word.length) < 40
+        trimmed_text.push(word)
+      else
+        break
+      end
     end
   end
 end
 
-def text_trim(text)
-  if text.length > 55
-    trimmed_text = []
-    collect_text(text, trimmed_text)
-    trimmed_text.join(" ") << "... <Read More>"
-  else
-    text
-  end
-end
-
-puts (text_trim(text))
+puts TextTrimmer.new(text).text_trim
