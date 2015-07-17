f5acf16681225a35e47e27bfbb768362160d9426
Michal Czyz <michalczyz@gmail.com>
2015-07-17 17:33:18 +0200

    Roman numbers object oriented version

--- code_eval_106/roman_numerals.rb
+++ code_eval_106/roman_numerals.rb
@@ -1,25 +1,39 @@
-require 'pry'
+class Roman
+  attr_reader :decimal
 
-roman_to_decimal = [
-    ['M', 1000],
-    ['CM', 900],
-    ['D', 500],
-    ['CD', 400],
-    ['C', 100],
-    ['XC', 90],
-    ['L', 50],
-    ['XL', 40],
-    ['X', 10],
-    ['IX', 9],
-    ['V', 5],
-    ['IV', 4],
-    ['I', 1]
-]
+  def initialize(decimal)
+    @decimal = decimal
+  end
 
-def decimal_to_roman(decimal_number, mapping)
-  return('') if mapping.empty?
-  symbol, divider = mapping.shift
-  (symbol * (decimal_number/divider)) + decimal_to_roman(decimal_number % divider, mapping)
+  def to_s
+    decimal_to_roman(decimal)
+  end
+
+  private
+
+  def decimal_to_roman(decimal_number)
+    return('') if mapping.empty?
+    symbol, divider = mapping.shift
+    (symbol * (decimal_number/divider)) + decimal_to_roman(decimal_number % divider)
+  end
+
+  def mapping
+    @mapping ||= [
+        ['M', 1000],
+        ['CM', 900],
+        ['D', 500],
+        ['CD', 400],
+        ['C', 100],
+        ['XC', 90],
+        ['L', 50],
+        ['XL', 40],
+        ['X', 10],
+        ['IX', 9],
+        ['V', 5],
+        ['IV', 4],
+        ['I', 1]
+    ]
+  end
 end
 
-puts decimal_to_roman(99, roman_to_decimal)
+puts Roman.new(99)
