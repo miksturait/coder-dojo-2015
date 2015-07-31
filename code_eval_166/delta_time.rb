103747966e7408fc1dbdcf495fada26901e31c1a
Michal Czyz <michalczyz@gmail.com>
2015-07-31 16:55:26 +0200

    Delta Time - Separating logic, introducing OurTime class.

--- code_eval_166/delta_time.rb
+++ code_eval_166/delta_time.rb
@@ -1,38 +1,33 @@
 DeltaTime = Struct.new(:start, :finish) do
   def to_s
-    seconds_to_time(delta())
+    (OurTime.parse(start) - OurTime.parse(finish)).to_s
   end
 
-  SECONDS_IN_HOUR = 3600
+  class OurTime < Struct.new(:hour, :minute, :second)
+    SECONDS_IN_HOUR = 3600
+    SECONDS_IN_MINUTE = 60
 
-  SECONDS_IN_MINUTE = 60
+    def self.new_from_seconds(second)
+      new(second / SECONDS_IN_HOUR,
+          (second % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE,
+          (second % SECONDS_IN_HOUR) % SECONDS_IN_MINUTE)
+    end
 
-  def time_to_seconds(time)
-    hour, minute, second = time.split(':').map(&:to_i)
-    hour*SECONDS_IN_HOUR + minute * SECONDS_IN_MINUTE + second
-  end
+    def self.parse(time)
+        new(*time.split(':').map(&:to_i))
+    end
 
-  def seconds_to_time(second)
-    format_time(second / SECONDS_IN_HOUR,
-                (second % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE,
-                (second % SECONDS_IN_HOUR) % SECONDS_IN_MINUTE)
-  end
+    def -(other_time)
+      self.class.new_from_seconds (to_i - other_time.to_i).abs
+    end
 
-  def format_time(hour, minutes, seconds)
-    [hour, minutes, seconds].map {|number| number.to_s.rjust(2, '0') }.join(':')
-  end
+    def to_s
+      [hour, minute, second].map { |number| number.to_s.rjust(2, '0') }.join(':')
+    end
 
-  def first_time()
-    time_to_seconds(start)
-  end
-
-  def second_time()
-    time_to_seconds(finish)
-  end
-
-  def delta()
-    (first_time()-second_time()).abs
-
+    def to_i
+      hour*SECONDS_IN_HOUR + minute * SECONDS_IN_MINUTE + second
+    end
   end
 end
 
