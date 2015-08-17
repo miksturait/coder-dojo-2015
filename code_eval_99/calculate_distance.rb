df44d7880ce403463afb6707b1dd667bbacbc00d
Michal Czyz <michalczyz@gmail.com>
2015-08-17 14:14:25 +0200

    calculate distance

---
+++ code_eval_99/calculate_distance.rb
@@ -0,0 +1,39 @@
+class Point < Struct.new(:x, :y);
+end
+
+class DistanceCalculation < Struct.new(:point_a, :point_b)
+  def to_s
+    distance.to_s
+  end
+
+  def distance
+    vector_z.to_i
+  end
+
+  private
+
+  def vector_z
+    Math.sqrt((vector_x**2) + (vector_y**2))
+  end
+
+  def vector_x
+    points.map(&:x).inject(:-)
+  end
+
+  def vector_y
+    points.map(&:y).inject(:-)
+  end
+
+  def points
+    [point_a, point_b]
+  end
+end
+
+require '../support/process_file'
+ProcessFile.new do |line|
+  coordinates = line.scan(/-?\d+/).map(&:to_i)
+
+  puts DistanceCalculation.new(
+           Point.new(coordinates.shift, coordinates.shift),
+           Point.new(coordinates.shift, coordinates.shift))
+end
