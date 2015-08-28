4f481c13741c6abb8ef8659394b6edd265427072
Michal Czyz <michalczyz@gmail.com>
2015-08-28 17:34:36 +0200

    Game of Life - Added neighbours system.

--- code_eval_161/game_of_life_tdd.rb
+++ code_eval_161/game_of_life_tdd.rb
@@ -1,10 +1,25 @@
 class GameOfLife < Struct.new(:generation_as_text)
+  VECTORS = [
+      [0, 1],
+      [0, -1],
+      [1, 0],
+      [-1, 0],
+      [-1, 1],
+      [1, 1],
+      [1, -1],
+      [-1, -1]
+  ]
+
   def generation_without_white_spaces
     generation_as_text.gsub(/\n/, '')
   end
 
   def world_size
-    @world_size ||= Math.sqrt(generation_without_white_spaces.length).to_i
+    @world_size ||= Math.sqrt(world_length).to_i
+  end
+
+  def world_length
+    @world_length ||= generation_without_white_spaces.length
   end
 
   def coordinate_to_index(x, y)
@@ -19,6 +34,22 @@ class GameOfLife < Struct.new(:generation_as_text)
 
   def [](x, y)
     generation_without_white_spaces[coordinate_to_index(x, y)]
+  end
+
+  def neighbours_indexes(index)
+    coords = index_to_coordinate(index)
+    neighbours_coords = VECTORS.map {|vx, vy| [coords[0] + vx, coords[1] + vy]   }
+    select_real_neighbours(neighbours_coords).map {|coords| coordinate_to_index(*coords)}
+  end
+
+  def select_real_neighbours(coordinates)
+       coordinates.select do |x, y|
+         in_world?(x) && in_world?(y)
+       end
+  end
+
+  def in_world?(y)
+    y.between?(0, world_size - 1)
   end
 end
 
@@ -39,5 +70,5 @@ end
 ProcessFile.new do |generation_as_text|
   game = GameOfLife.new(generation_as_text)
   puts game.world_size
-  p game[1, 8]
+  p game.neighbours_indexes(0)
 end
