96c3e4131e718733335e3445b598ad36974c96cc
Michal Czyz <michalczyz@gmail.com>
2015-08-28 17:11:44 +0200

    Game of Life - Working on string like two dimential table.

--- code_eval_161/game_of_life_tdd.rb
+++ code_eval_161/game_of_life_tdd.rb
@@ -1,8 +1,32 @@
+class GameOfLife < Struct.new(:generation_as_text)
+  def generation_without_white_spaces
+    generation_as_text.gsub(/\n/, '')
+  end
+
+  def world_size
+    @world_size ||= Math.sqrt(generation_without_white_spaces.length).to_i
+  end
+
+  def coordinate_to_index(x, y)
+     y * world_size + x
+  end
+
+  def index_to_coordinate(index)
+      y = index / world_size
+      x = index % world_size
+    [x,y]
+  end
+
+  def [](x, y)
+    generation_without_white_spaces[coordinate_to_index(x, y)]
+  end
+end
+
 
 class ProcessFile
   def initialize(filename = ARGV[0])
     @filename = filename
-     yield(file_body())
+    yield(file_body())
   end
 
   private
@@ -13,5 +37,7 @@ class ProcessFile
 end
 
 ProcessFile.new do |generation_as_text|
-   puts generation_as_text
+  game = GameOfLife.new(generation_as_text)
+  puts game.world_size
+  p game[1, 8]
 end
