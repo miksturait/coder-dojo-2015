67910a825efe7967c7a2b38ad755020d8985b3b9
Michal Czyz <michalczyz@gmail.com>
2015-08-28 16:10:29 +0200

    Find the highest score - Ref

--- code_eval_208/highest_score.rb
+++ code_eval_208/highest_score.rb
@@ -1,9 +1,29 @@
+class HighScoreFinder < Struct.new(:line)
+  def players_input
+    line.split(" | ")
+  end
+
+  def scores_per_player
+     players_input.map do |player_scores|
+       player_scores.split(" ").map(&:to_i)
+     end
+  end
+
+  def scores_per_category
+    scores_per_player.transpose
+  end
+
+  def max_score_per_category
+    scores_per_category.map(&:max)
+  end
+
+  def to_s
+    max_score_per_category.join(' ')
+  end
+
+end
+
 require '../support/process_file'
 ProcessFile.new do |line|
-  puts (line.
-           split(" | ").
-           map {|player_scores| player_scores.split(" ").map(&:to_i)}.
-           transpose.
-           map(&:max).
-           join(" ")    )
+  puts HighScoreFinder.new(line)
 end
