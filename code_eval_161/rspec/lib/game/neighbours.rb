33f37a612f66e5c7372f907b149e124124e10e29
Michal Czyz <michalczyz@gmail.com>
2015-09-04 16:31:37 +0200

    Implemented Rspec.

--- code_eval_161/rspec/lib/game/neighbours.rb
+++ code_eval_161/rspec/lib/game/neighbours.rb
@@ -1,4 +1,4 @@
-class Game::Neighbours < Struct.new(:coordinate, :word_size)
+class Game::Neighbours < Struct.new(:coordinate, :world_size)
   VECTORS = [
       [0, 1],
       [0, -1],
@@ -11,7 +11,7 @@ class Game::Neighbours < Struct.new(:coordinate, :word_size)
   ]
 
   def reachable
-    []
+    select_real_neighbours(neighbours_coordinates(coordinate))
   end
 
   private
