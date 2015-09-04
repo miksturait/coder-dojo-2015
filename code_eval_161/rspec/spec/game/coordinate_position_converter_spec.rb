3f30f2b853f8470eff54a69680d31e654609f4ea
Michal Czyz <michalczyz@gmail.com>
2015-09-04 18:40:43 +0200

    Group expectation for each method.

--- code_eval_161/rspec/spec/game/coordinate_position_converter_spec.rb
+++ code_eval_161/rspec/spec/game/coordinate_position_converter_spec.rb
@@ -1,12 +1,16 @@
 describe Game::CoordinatePositionConverter do
-  let(:converter) {described_class.new(10)}
+  let(:converter) { described_class.new(10) }
 
-  it {expect(converter.to_index([0,0])).to eq(0)}
-  it {expect(converter.to_index([9,9])).to eq(99)}
-  it {expect(converter.to_index([0,1])).to eq(10)}
-  it {expect(converter.to_index([1,0])).to eq(1)}
+  describe '#to_index' do
+    it { expect(converter.to_index([0, 0])).to eq(0) }
+    it { expect(converter.to_index([9, 9])).to eq(99) }
+    it { expect(converter.to_index([0, 1])).to eq(10) }
+    it { expect(converter.to_index([1, 0])).to eq(1) }
+  end
 
-  it {expect(converter.to_coordinate(99)).to eq([9,9])}
-  it {expect(converter.to_coordinate(0)).to eq([0,0])}
-  it {expect(converter.to_coordinate(13)).to eq([3,1])}
+  describe '#to_coordinate' do
+    it { expect(converter.to_coordinate(99)).to eq([9, 9]) }
+    it { expect(converter.to_coordinate(0)).to eq([0, 0]) }
+    it { expect(converter.to_coordinate(13)).to eq([3, 1]) }
+  end
 end
