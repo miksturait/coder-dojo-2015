53107bfddf85d659ff987cc98caae68898a44acb
Michal Czyz <michalczyz@gmail.com>
2015-09-04 13:51:26 +0200

    configuring rspec

---
+++ code_eval_161/rspec/spec/spec_helper.rb
@@ -0,0 +1,23 @@
+require 'init'
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.warnings = true
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  end
+
+  config.example_status_persistence_file_path = 'tmp/last_run.txt'
+
+  config.profile_examples = 10
+
+  config.order = :random
+  Kernel.srand config.seed
+end
