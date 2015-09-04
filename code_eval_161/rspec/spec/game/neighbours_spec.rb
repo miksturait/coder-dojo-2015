49952974f68e35cea9d9a27224f048cce797f754
Michal Czyz <michalczyz@gmail.com>
2015-09-04 18:37:54 +0200

    Added test examples.

--- code_eval_161/rspec/spec/game/neighbours_spec.rb
+++ code_eval_161/rspec/spec/game/neighbours_spec.rb
@@ -1,5 +1,4 @@
 describe Game::Neighbours do
-  let(:index) { 10 }
   #..*.
   #....
   #****
@@ -8,8 +7,17 @@ describe Game::Neighbours do
   let(:world_dimension) {4}
   let(:neighbours) {described_class.new(index, world, world_dimension)}
 
-  it {expect(neighbours.live_amount).to eq(5)}
+  context 'position 10' do
+    let(:index) { 10 }
 
-  it {expect(neighbours.all).to match_array([9, 11, 5, 6, 7, 15, 13, 14])}
+    it {expect(neighbours.live_amount).to eq(5)}
+    it {expect(neighbours.all).to match_array([9, 11, 5, 6, 7, 15, 13, 14])}
+  end
 
+  context 'position 2' do
+    let(:index) { 2 }
+
+    it {expect(neighbours.live_amount).to eq(0)}
+    it {expect(neighbours.all).to match_array([1, 3, 5, 6, 7])}
+  end
 end
