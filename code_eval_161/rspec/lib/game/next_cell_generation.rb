aa08d5e8d733970b5abe11df86a50da4dcec96e4
Michal Czyz <michalczyz@gmail.com>
2015-09-11 17:23:16 +0200

    Implementation Game::NextCellGeneration Any live cell with two or three live neighbors lives on to the next generation two should eq "*"

--- code_eval_161/rspec/lib/game/next_cell_generation.rb
+++ code_eval_161/rspec/lib/game/next_cell_generation.rb
@@ -1,5 +1,11 @@
 class Game::NextCellGeneration < Struct.new(:current_state, :live_neighbours_count)
   def next_state
-    '.'
+    if current_state == '*'
+      if live_neighbours_count == 2 || live_neighbours_count == 3
+        '*'
+      else
+        '.'
+      end
+    end
   end
 end
