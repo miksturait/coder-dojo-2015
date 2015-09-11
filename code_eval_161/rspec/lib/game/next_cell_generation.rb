be9c2cf94500e25c5b563453c7ee228215588e75
Michal Czyz <michalczyz@gmail.com>
2015-09-11 17:51:46 +0200

    Game::NextCellGeneration refactoring next_state method

--- code_eval_161/rspec/lib/game/next_cell_generation.rb
+++ code_eval_161/rspec/lib/game/next_cell_generation.rb
@@ -1,17 +1,31 @@
 class Game::NextCellGeneration < Struct.new(:current_state, :live_neighbours_count)
   def next_state
-    if current_state == '*'
-      if live_neighbours_count == 2 || live_neighbours_count == 3
+    if is_alive
+      if two_alive || three_alive
         '*'
       else
         '.'
       end
     else
-      if live_neighbours_count == 3
+      if three_alive
         '*'
       else
         '.'
       end
     end
+  end
+
+  private
+
+  def is_alive
+    current_state == '*'
+  end
+
+  def two_alive
+    live_neighbours_count == 2
+  end
+
+  def three_alive
+    live_neighbours_count == 3
   end
 end
