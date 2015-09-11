71f285f4d15fba2566d2f5a35c1d5107f494e27a
Michal Czyz <michalczyz@gmail.com>
2015-09-11 19:02:04 +0200

    specGame::ofLife spec

--- code_eval_161/rspec/spec/game/of_life_spec.rb
+++ code_eval_161/rspec/spec/game/of_life_spec.rb
@@ -1,10 +1,9 @@
 describe Game::OfLife do
   let(:initial_state) { "00000\nf0000\ne0000\na0000\n0b000\n" }
-  let(:world) { described_class.new(initial_state)}
+  let(:world) { described_class.new(initial_state )}
 
   it { expect(world.dimension).to eq(5) }
 
-  it "get 'a' on [3,1]"
-
-  it "get 'b' on [5]"
+  it 'next generation world'
+  it 'nth generation world'
 end
