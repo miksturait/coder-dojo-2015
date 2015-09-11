9bba2d5bd041d0c5c6a6de2b2d245d9d78b0f668
Michal Czyz <michalczyz@gmail.com>
2015-09-11 18:27:42 +0200

    Game::NextCellGeneration refactoring next_state method simplifying logic operations cleanup

--- code_eval_161/rspec/spec/game/next_cell_generation_spec.rb
+++ code_eval_161/rspec/spec/game/next_cell_generation_spec.rb
@@ -3,11 +3,13 @@ describe Game::NextCellGeneration do
 
   context 'Any live cell' do
     let(:current_state) { '*' }
+
     context 'with fewer than two live neighbors dies, as if caused by under-population' do
       context 'one' do
         let(:live_neighbours_count) { 1 }
         it { expect(next_generation_service.next_state).to eq('.') }
       end
+
       context 'zero' do
         let(:live_neighbours_count) { 0 }
         it { expect(next_generation_service.next_state).to eq('.') }
@@ -19,6 +21,7 @@ describe Game::NextCellGeneration do
         let(:live_neighbours_count) { 2 }
         it { expect(next_generation_service.next_state).to eq('*') }
       end
+
       context 'three' do
         let(:live_neighbours_count) { 3 }
         it { expect(next_generation_service.next_state).to eq('*') }
@@ -30,6 +33,7 @@ describe Game::NextCellGeneration do
         let(:live_neighbours_count) { 4 }
         it { expect(next_generation_service.next_state).to eq('.') }
       end
+
       context 'eight' do
         let(:live_neighbours_count) { 8 }
         it { expect(next_generation_service.next_state).to eq('.') }
@@ -39,21 +43,20 @@ describe Game::NextCellGeneration do
 
   context 'Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction' do
     let(:current_state) { '.' }
+
     context 'three' do
       let(:live_neighbours_count) { 3 }
       it { expect(next_generation_service.next_state).to eq('*') }
     end
+
     context 'two' do
       let(:live_neighbours_count) { 2 }
       it { expect(next_generation_service.next_state).to eq('.') }
-
     end
+
     context 'four' do
       let(:live_neighbours_count) { 4 }
       it { expect(next_generation_service.next_state).to eq('.') }
     end
-
   end
-
 end
-
