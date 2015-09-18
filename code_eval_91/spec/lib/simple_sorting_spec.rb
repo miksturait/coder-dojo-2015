11331d4b1e34d94b511461a1f60a9402905b06dc
Michal Czyz <michalczyz@gmail.com>
2015-09-18 17:21:51 +0200

    Simple Sorting - added second test.

--- code_eval_91/spec/lib/simple_sorting_spec.rb
+++ code_eval_91/spec/lib/simple_sorting_spec.rb
@@ -1,6 +1,13 @@
 describe SimpleSorting do
-  let(:numbers_as_strings) {%w(70.920 -38.797 14.354 99.323 90.374 7.581)}
-  let(:sorting_object) {described_class.new(numbers_as_strings)}
+  let(:sorting_object) { described_class.new(numbers_as_strings) }
 
-  it {expect(sorting_object.to_s).to eq('-38.797 7.581 14.354 70.920 90.374 99.323')}
+  context "3'rd zero is added" do
+    let(:numbers_as_strings) { %w(70.920 -38.797 14.354 99.323 90.374 7.581) }
+    it { expect(sorting_object.to_s).to eq('-38.797 7.581 14.354 70.920 90.374 99.323') }
+  end
+
+  context "4'th or next zero are cutted" do
+    let(:numbers_as_strings) { %w(70.920123 11.0000) }
+    it { expect(sorting_object.to_s).to eq('11.000 70.920') }
+  end
 end
