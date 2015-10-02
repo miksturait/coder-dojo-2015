1efa47d02f28e9999dc3b233b8175436b1af1667
Michal Czyz <michalczyz@gmail.com>
2015-10-02 19:10:03 +0200

    Cash Register - first probably working solution.

--- code_eval_54/lib/cash_register.rb
+++ code_eval_54/lib/cash_register.rb
@@ -1,11 +1,46 @@
 class CashRegister < Struct.new(:purchase_price, :customer_cash)
   def odd_money
     if purchase_price < customer_cash
-      (customer_cash - purchase_price).round(2)
+      coins
     elsif purchase_price == customer_cash
       'ZERO'
     else
       'ERROR'
     end
   end
+
+  def coins
+    x(reminder).join(',')
+  end
+
+  def reminder
+     (customer_cash - purchase_price).round(2)
+  end
+
+  def x(decimal_number)
+    decimal_number = decimal_number.round(2)
+    return([]) if mapping.empty?
+    symbol, divider = mapping.shift
+    ((decimal_number/divider).to_i.times.map {symbol.to_s}).concat x(decimal_number % divider)
+  end
+
+  def mapping
+    @mapping ||= {
+        'PENNY': 0.01,
+        'NICKEL': 0.05,
+        'DIME': 0.10,
+        'QUARTER': 0.25,
+        'HALF DOLLAR': 0.50,
+        'ONE': 1.00,
+        'TWO': 2.00,
+        'FIVE': 5.00,
+        'TEN': 10.00,
+        'TWENTY': 20.00,
+        'FIFTY': 50.00,
+        'ONE HUNDRED': 100.00
+    }.to_a.reverse
+  end
+
+
+
 end
--- code_eval_54/spec/lib/cash_register_spec.rb
+++ code_eval_54/spec/lib/cash_register_spec.rb
@@ -2,11 +2,11 @@ describe CashRegister do
 
   let(:register) { described_class.new(pp, cc) }
 
-  describe 'rest' do
+  describe 'rest calculation' do
     context 'PP<CC' do
       let(:pp) { 15.94 }
       let(:cc) { 16.00 }
-      it { expect(register.odd_money).to eq(0.06) }
+      it { expect(register.odd_money).to eq('NICKEL,PENNY') }
     end
 
     context 'PP>CC' do
@@ -19,6 +19,14 @@ describe CashRegister do
       let(:pp) { 17.00 }
       let(:cc) { 17.00 }
       it { expect(register.odd_money).to eq('ZERO') }
+    end
+  end
+
+  describe 'change odd to cash' do
+    context 'to one coin' do
+      let(:pp) { 16.99 }
+      let(:cc) { 17.00 }
+      it { expect(register.coins).to eq('PENNY') }
     end
   end
 end
