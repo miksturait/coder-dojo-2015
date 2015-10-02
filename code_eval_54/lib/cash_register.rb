10199fbc26a0ccb4967b215d2a388aab11a093f7
Michal Czyz <michalczyz@gmail.com>
2015-10-02 18:11:10 +0200

    Cash Register - calculating odd money first solution.

--- code_eval_54/lib/cash_register.rb
+++ code_eval_54/lib/cash_register.rb
@@ -1,3 +1,11 @@
-class CashRegister < Struct.new(:purchase_price, :customer_money)
-
+class CashRegister < Struct.new(:purchase_price, :customer_cash)
+  def odd_money
+    if purchase_price < customer_cash
+      (customer_cash - purchase_price).round(2)
+    elsif purchase_price == customer_cash
+      'ZERO'
+    else
+      'ERROR'
+    end
+  end
 end
--- code_eval_54/spec/lib/cash_register_spec.rb
+++ code_eval_54/spec/lib/cash_register_spec.rb
@@ -1,18 +1,24 @@
 describe CashRegister do
 
-  let(:register) {described_class.new(15.94, 16.00)}
+  let(:register) { described_class.new(pp, cc) }
 
-  context 'PP<CC' do
-    it {expect(register.odd_money).to eq(0.06)}
+  describe 'rest' do
+    context 'PP<CC' do
+      let(:pp) { 15.94 }
+      let(:cc) { 16.00 }
+      it { expect(register.odd_money).to eq(0.06) }
+    end
+
+    context 'PP>CC' do
+      let(:pp) { 17.00 }
+      let(:cc) { 16.00 }
+      it { expect(register.odd_money).to eq('ERROR') }
+    end
+
+    context 'PP=CC' do
+      let(:pp) { 17.00 }
+      let(:cc) { 17.00 }
+      it { expect(register.odd_money).to eq('ZERO') }
+    end
   end
-
-  context 'PP>CC' do
-
-  end
-
-  context 'PP=CC' do
-
-  end
-
-
 end
